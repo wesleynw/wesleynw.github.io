5c1efe0621e381c27e09c2a7856b1d3d7f3d744d
Wesley Weisenberger <wesley@berkeley.edu>
2021-11-29 17:22:11 -0800

    animation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,16 @@
 let arrow = document.getElementById("arrow");
-anime({
+
+var arrow_animation = anime({
     targets: '#arrow',
     loop: true,
     translateY: '20px',
     direction: 'alternate',
     duration: 1600,
-    easing: 'cubicBezier(0.445, 0.05, 0.55, 0.95)'
+    easing: 'cubicBezier(0.445, 0.05, 0.55, 0.95)',
+    autoplay: true
 })
+
+// document.getElementById("arrow").onmouseenter = arrow_animation.play;
+// document.getElementById("arrow").onmouseleave = arrow_animation.play;
+document.querySelector("#arrow").onmouseenter = arrow_animation.pause;
+document.querySelector("#arrow").onmouseleave = arrow_animation.play;
