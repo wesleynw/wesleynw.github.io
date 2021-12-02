23f304b0a2b375e1736141248b536bd9c14996eb
Wesley Weisenberger <wesley@berkeley.edu>
2021-12-01 19:07:16 -0800

    more responsive navbar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,12 +13,13 @@ var arrow_animation = anime({
 document.querySelector("#arrow").onmouseenter = arrow_animation.pause;
 document.querySelector("#arrow").onmouseleave = arrow_animation.play;
 
-let leftPosition = false;
-let widthCorrect = document.getElementById("navbarMain").offsetWidth;
-let dist = -window.innerWidth / 2 + widthCorrect / 2 + 20
 
+let leftPosition = false;
+let navHidden = false;
 
 window.onscroll = function() {
+    let widthCorrect = document.getElementById("navbarMain").offsetWidth;
+    let dist = -window.innerWidth / 2 + widthCorrect / 2 + 20
     if (window.scrollY === 0) {
         document.getElementById("navbar").style.backgroundColor = "";
 
@@ -26,23 +27,33 @@ window.onscroll = function() {
         document.getElementById("navbar").style.backgroundColor = "#121212";
     }
     if (window.scrollY >= 350) {
-        if (window.innerWidth <= 600) {
+        if (window.innerWidth <= 600 ||
+            (document.getElementById("navbarMain").offsetWidth + (document.getElementById("nav-name").offsetWidth / 2) > (window.innerWidth / 2))) {
             document.getElementById("navbarMain").style.display = "none";
+            navHidden = true;
         }
         document.getElementById("nav-name").style.display = "inline";
         anime({
             targets: '#navbarMain',
             translateX: dist,
             duration: 250
-        })
+        });
+        anime({
+            targets: '#navbarMain a',
+            marginRight: 5,
+            marginLeft: 5,
+            fontSize: "10pt",
+            fontSize: "13pt",
+            duration: 250
+        });
         anime({
             targets: '#nav-name',
             opacity: 1,
             duration: 500
-        })
+        });
         leftPosition = true;
     } else if (leftPosition === true && window.scrollY <= 350) {
-        if (window.innerWidth <= 600) {
+        if (navHidden) {
             document.getElementById("navbarMain").style.display = "inline";
         }
         document.getElementById("nav-name").style.display = "none";
@@ -55,6 +66,13 @@ window.onscroll = function() {
             targets: '#nav-name',
             opacity: 0,
             duration: 500
+        })
+        anime({
+            targets: '#navbar a',
+            marginRight: 15,
+            marginLeft: 15,
+            fontSize: "3vh",
+            duration: 250
         })
     }
 }
