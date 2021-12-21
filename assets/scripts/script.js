e82aa311d30035e8ef238fdd9742a8892cefd604
Wesley Weisenberger <wesley@berkeley.edu>
2021-12-20 16:07:18 -0800

    fixed some animations

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,79 +1,30 @@
 let arrow = document.getElementById("arrow");
+let arrowPath = document.getElementById("arrow-path");
 
-var arrow_animation = anime({
+var arrowAnimation = anime({
     targets: '#arrow',
     loop: true,
     translateY: '20px',
     direction: 'alternate',
-    duration: 1600,
+    duration: 1300,
     easing: 'cubicBezier(0.445, 0.05, 0.55, 0.95)',
     autoplay: true
 })
-
-document.querySelector("#arrow").onmouseenter = arrow_animation.pause;
-document.querySelector("#arrow").onmouseleave = arrow_animation.play;
-
-
-let leftPosition = false;
-let navHidden = false;
+arrow.onmouseenter = function() {
+    arrowAnimation.pause();
+    arrowPath.style.opacity = 0.85;
+}
+arrow.onmouseleave = function() {
+    arrowAnimation.play();
+    arrowPath.style.opacity = 1;
+}
 
 window.onscroll = function() {
-    let widthCorrect = document.getElementById("navbarMain").offsetWidth;
-    let dist = -window.innerWidth / 2 + widthCorrect / 2 + 20
     if (window.scrollY === 0) {
         document.getElementById("nav").style.backgroundColor = "";
 
     } else {
-        document.getElementById("nav").style.backgroundColor = "#121212";
-    }
-    if (window.scrollY >= 350) {
-        if (window.innerWidth <= 600 ||
-            (document.getElementById("navbarMain").offsetWidth + (document.getElementById("nav-name").offsetWidth / 2) > (window.innerWidth / 2))) {
-            document.getElementById("navbarMain").style.display = "none";
-            navHidden = true;
-        }
-        document.getElementById("nav-name").style.display = "inline";
-        anime({
-            targets: '#navbarMain',
-            translateX: dist,
-            duration: 250
-        });
-        anime({
-            targets: '#navbarMain a',
-            marginRight: 5,
-            marginLeft: 5,
-            fontSize: "10pt",
-            fontSize: "13pt",
-            duration: 250
-        });
-        anime({
-            targets: '#nav-name',
-            opacity: 1,
-            duration: 500
-        });
-        leftPosition = true;
-    } else if (leftPosition === true && window.scrollY <= 350) {
-        if (navHidden) {
-            document.getElementById("navbarMain").style.display = "inline";
-        }
-        document.getElementById("nav-name").style.display = "none";
-        anime({
-            targets: '#navbarMain',
-            translateX: 0,
-            duration: 250
-        })
-        anime({
-            targets: '#nav-name',
-            opacity: 0,
-            duration: 500
-        })
-        anime({
-            targets: '#navbar a',
-            marginRight: 15,
-            marginLeft: 15,
-            fontSize: "3vh",
-            duration: 250
-        })
+        document.getElementById("nav").style.backgroundColor = "#C0E6C5";
     }
 }
 
